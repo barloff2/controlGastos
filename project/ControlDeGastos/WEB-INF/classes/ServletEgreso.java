db9cd82655b7ae7faa4f85e99e9b8b939a5f0ad8
Stiven Parada <barloff2@gmail.com>
2022-10-11 22:03:09 -0500

    Compilado archivos de classes

--- project/ControlDeGastos/WEB-INF/classes/ServletEgreso.java
+++ project/ControlDeGastos/WEB-INF/classes/ServletEgreso.java
@@ -1,12 +1,12 @@
 package vista;
 
-import javax.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServlet;
 import java.io.IOException;
-import javax.servlet.Servlet;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.Servlet;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 /**
  * Servlet implementation class ServletEgreso
