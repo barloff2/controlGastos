4d150c27ba300a9572ea9c6f3f506e470cad11b2
Stiven <barloff2@gmail.com>
2022-10-22 00:27:28 -0500

    cambios 22 10 22

--- src/main/java/gestion/controlador/ServletEstado.java
+++ src/main/java/gestion/controlador/ServletEstado.java
@@ -13,11 +13,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.Gson;
+
 import gestion.DAO.GastoDAO;
 import gestion.DAO.IngresoDAO;
 import gestion.modelo.Gasto;
 import gestion.modelo.Ingreso;
-
 
 /**
  * Servlet implementation class ServletEstado
@@ -27,26 +28,23 @@ public class ServletEstado extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest peticion, HttpServletResponse respuesta)
-			throws ServletException, IOException {
-		// RequestDispatcher rd = peticion.getRequestDispatcher("Estado.html");
-
-	}
-
-	protected void processRequest(HttpServletRequest peticion, HttpServletResponse respuesta)
-			throws ServletException, IOException {
-		respuesta.setContentType("text/html;charset=UTF-8");
+			throws ServletException, IOException {	
 		String tipo = peticion.getParameter("tipo");
 		String json = "";
+		Gson gson = new Gson();
 
 
 		switch (tipo) {
-			case "ingreso":
+			case "Ingresos":
 				List<Ingreso> listaI = new ArrayList();
 				IngresoDAO ingresoDao = new DAOIngresoImpl();
 				try {
 					listaI = ingresoDao.mostrarIngresos();
-					ObjectMapper mapper = new ObjectMaper();
-					json = mapper.writeValuesAsString(listaI);
+					
+					for (Ingreso ingreso : listaI) {
+						json += gson.toJson(ingreso);
+						
+					}
 					try (PrintWriter out = respuesta.getWriter()){
 						System.out.println(json);
 					};
@@ -61,11 +59,14 @@ public class ServletEstado extends HttpServlet {
 				}
 				break;
 			
-			case "gasto":
+			case "Gastos":
 				List<Gasto> listaG = new ArrayList();
 				GastoDAO gastoDao = new DAOGastoImpl();
 				try {
 					listaG = gastoDao.mostrarGastos();
+					for (Gasto gasto : listaG) {
+						json += gson.toJson(gasto);
+					}
 				} catch (Exception e1) {
 					System.out.println(e1.getMessage());
 				}
@@ -74,17 +75,48 @@ public class ServletEstado extends HttpServlet {
 				} catch (Exception e) {
 					System.out.println(e.getMessage());
 				}
-				json = 
+				
 				break;
 			default:
 				break;
 		}
 
 
-		try (PrintWriter out = respuesta.getWriter()) {
-			
-			out.print(lista);
-		}
+		PrintWriter salida = respuesta.getWriter();
+
+		salida.println("<!DOCTYPE html>");
+		salida.println("<head>");
+		salida.println("<meta charset= \"utf-8\" >");
+		salida.println("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">");
+
+		salida.println("<title> Registrar Ingresos </title>");
+
+		salida.println("<script type=module src=main.js></script>");
+
+		salida.println(
+				"<link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css\" integrity=\"sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N\" crossorigin=\"anonymous\"> ");
+		salida.println("</head>");
+		salida.println("<mn-header>");
+		salida.println("</mn-header>");
+		salida.println("<div class=\"alert alert-success\" role=\"alert\"> Ingreso Exitoso! </div>");
+		salida.println("<ul class=\"list-group\">");
+		salida.println(" <li class=\"list-group-item\">Saldo actual: " + json);
+		salida.println("</li>");
+		salida.println("<li class=\"list-group-item\">Razon: ");
+		salida.println("</li>");
+		salida.println(" <li class=\"list-group-item\">Fecha: ");
+		salida.println(" </li>");
+		salida.println("  <li class=\"list-group-item\">Responsable: " );
+		salida.println(" </li>");
+		salida.println(" </ul>");
+		salida.println(" <mn-footer></mn-footer>");
+		salida.println("  </body>");
+		salida.println(" </html>");
+
+	}
+
+	protected void processRequest(HttpServletRequest peticion, HttpServletResponse respuesta)
+			throws ServletException, IOException {
 
 	}
 
